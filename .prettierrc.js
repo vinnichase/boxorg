72c462d9e28916de379662d0063d14f08a650a3e
vinnichase <vincenz.koop@gmail.com>
2025-02-05 08:29:54 +0100

    formatting

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+/** @format */
+
+module.exports = {
+    bracketSpacing: true,
+    endOfLine: 'auto',
+    insertPragma: false,
+    printWidth: 120,
+    proseWrap: 'always',
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    tabWidth: 4,
+    trailingComma: 'all',
+    htmlWhitespaceSensitivity: 'ignore',
+    plugins: [],
+};
