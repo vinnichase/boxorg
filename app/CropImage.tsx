6eeb0311b7e3b272fa204b79042cc064fba16e85
vinnichase <vincenz.koop@gmail.com>
2025-02-07 07:11:14 +0100

    improved crop image

--- app/CropImage.tsx
+++ app/CropImage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { View, Image, Button } from 'react-native';
+import { Image } from 'react-native';
 import ImageEditor from '@react-native-community/image-editor';
 
 type CropImageProps = {
@@ -34,9 +34,22 @@ const CropImage = ({ base64, rect }: CropImageProps) => {
     return croppedImage && <Image source={{ uri: croppedImage }} style={{ width: 200, height: 200 }} />;
 };
 
+/**
+ * Get square dimensions with 5% padding but without moving the crop area outside the image
+ */
 const getSquareDimensions = (x: number, y: number, w: number, h: number) => {
+    const padding = 0.05;
     const size = Math.max(w, h);
-    return [x + (w - size) / 2, y + (h - size) / 2, size, size];
+    const halfSize = size / 2;
+    const centerX = x + w / 2;
+    const centerY = y + h / 2;
+    const halfPadding = (size * padding) / 2;
+    return [
+        Math.max(0, centerX - halfSize - halfPadding),
+        Math.max(0, centerY - halfSize - halfPadding),
+        size + halfPadding * 2,
+        size + halfPadding * 2,
+    ];
 };
 
 export default CropImage;
