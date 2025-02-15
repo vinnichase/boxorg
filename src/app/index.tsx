9c8581e983f65070cf681870cea799b002d56fb0
vinnichase <vincenz.koop@gmail.com>
2025-02-15 09:16:11 +0100

    aperture styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,8 @@ import { ImageBackground, SafeAreaView, ScrollView, StyleSheet, Text, TouchableO
 import * as ImagePicker from 'expo-image-picker';
 import { RNMLKitObjectDetectionObject, useObjectDetector } from '@infinitered/react-native-mlkit-object-detection';
 import CropImage from '../components/CropImage';
-import { PURPLE_DARK, RED, WHITE } from '../util/constants';
+import { BLACK, NONE, PURPLE_DARK, PURPLE_MID, RED, WHITE } from '../util/constants';
+import Svg, { Circle, Line, Polygon } from 'react-native-svg';
 
 function App(): JSX.Element {
     const [image, setImage] = useState<ImagePicker.ImagePickerAsset | null>();
@@ -57,7 +58,7 @@ function App(): JSX.Element {
 
     return (
         <ImageBackground
-            source={require('../../assets/images/background.png')} // Replace with your image
+            source={require('../../assets/images/background.png')}
             style={{
                 flex: 1,
                 backgroundColor: PURPLE_DARK,
@@ -84,11 +85,13 @@ function App(): JSX.Element {
                 </ScrollView>
                 <TouchableOpacity
                     style={{
-                        width: 100,
-                        height: 100,
+                        width: 110,
+                        height: 110,
                         margin: 20,
-                        padding: 5,
+                        padding: 4,
                         backgroundColor: WHITE,
+                        boxShadow: `0 0 80px 10px ${BLACK}bb`,
+                        opacity: 0.9,
                         alignSelf: 'center',
                         borderRadius: '100%',
                         overflow: 'hidden',
@@ -102,8 +105,24 @@ function App(): JSX.Element {
                             borderRadius: '100%',
                             borderColor: RED,
                             borderWidth: 1,
+                            padding: 13,
                         }}
-                    ></View>
+                    >
+                        <Svg style={{}} viewBox="-2 -2 101 101">
+                            <Circle stroke={PURPLE_DARK} strokeWidth={3} fill={NONE} cx="48.5" cy="48.5" r="47.5" />
+                            <Polygon
+                                fill={PURPLE_DARK}
+                                points="48.5 29.01 33.26 36.35 29.5 52.84 40.04 66.06 56.96 66.06 67.5 52.84 63.74 36.35 48.5 29.01"
+                            />
+                            <Line stroke={PURPLE_DARK} strokeWidth={3} x1="34.77" y1="93.88" x2="67.5" y2="52.84" />
+                            <Line stroke={PURPLE_DARK} strokeWidth={3} x1="4.35" y1="66.06" x2="56.96" y2="66.06" />
+                            <Line stroke={PURPLE_DARK} strokeWidth={3} x1="7.25" y1="24.94" x2="40.04" y2="66.06" />
+                            <Line stroke={PURPLE_DARK} strokeWidth={3} x1="41.2" y1="1.56" x2="29.5" y2="52.84" />
+                            <Line stroke={PURPLE_DARK} strokeWidth={3} x1="80.64" y1="13.53" x2="33.26" y2="36.35" />
+                            <Line stroke={PURPLE_DARK} strokeWidth={3} x1="95.88" y1="51.83" x2="48.5" y2="29.01" />
+                            <Line stroke={PURPLE_DARK} strokeWidth={3} x1="75.44" y1="87.62" x2="63.74" y2="36.35" />
+                        </Svg>
+                    </View>
                 </TouchableOpacity>
             </SafeAreaView>
         </ImageBackground>
--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,4 +1,7 @@
 export const PURPLE_DARK = '#23153a';
 export const PURPLE_MID = '#442871';
 export const RED = 'red';
-export const WHITE = 'white';
+export const BLACK = '#0c0714';
+export const WHITE = '#ffffff';
+
+export const NONE = 'none';
