b4ea844ab6f243cfa04551f84e97e5a3e4de0d3d
vinnichase <vincenz.koop@gmail.com>
2025-02-20 20:30:20 +0100

    factored out icons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,11 @@
 import React, { useState } from 'react';
-import {
-    Animated,
-    ImageBackground,
-    Keyboard,
-    SafeAreaView,
-    ScrollView,
-    TextInput,
-    TouchableOpacity,
-    View,
-} from 'react-native';
+import { Animated, ImageBackground, Keyboard, SafeAreaView, TextInput, TouchableOpacity, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { RNMLKitObjectDetectionObject, useObjectDetector } from '@infinitered/react-native-mlkit-object-detection';
-import CropImage from '../components/CropImage';
 import { BLACK, NONE, PURPLE_DARK, PURPLE_MID, RED, WHITE } from '../util/constants';
 import Svg, { Circle, Line, Path, Polygon, Rect } from 'react-native-svg';
 import AnimatedBlurView from '../components/AnimatedBlurView';
+import { ApertureIcon, BoxIcon, LoopIcon } from '../components/Icons';
 
 function App(): JSX.Element {
     const [image, setImage] = useState<ImagePicker.ImagePickerAsset | null>();
@@ -109,19 +100,6 @@ function App(): JSX.Element {
                         onUnshift();
                     }}
                 >
-                    {/* <ScrollView>
-                    {image &&
-                        result.map((obj, index) => (
-                            <View key={index}>
-                                <CropImage
-                                    key={index}
-                                    image={image}
-                                    rect={[obj.frame.origin.x, obj.frame.origin.y, obj.frame.size.x, obj.frame.size.y]}
-                                />
-                                <Text>{obj.labels.map((l) => l.text).join(',')}</Text>
-                            </View>
-                        ))}
-                </ScrollView> */}
                     <View style={{ flex: 2 }}></View>
                     <Animated.View
                         style={{
@@ -147,37 +125,7 @@ function App(): JSX.Element {
                                 gap: 10,
                             }}
                         >
-                            <View>
-                                <Svg
-                                    style={{
-                                        opacity: 0.9,
-                                        aspectRatio: 1,
-                                        // borderWidth: 1,
-                                    }}
-                                    viewBox="-1 -1 31.71 31.71"
-                                >
-                                    <Path
-                                        fill={NONE}
-                                        strokeWidth={2}
-                                        stroke={PURPLE_DARK}
-                                        d="M30.21,30.21l-9.47-9.47"
-                                    />
-                                    <Circle
-                                        fill={NONE}
-                                        strokeWidth={2}
-                                        stroke={PURPLE_DARK}
-                                        cx="12.85"
-                                        cy="12.85"
-                                        r="11.35"
-                                    />
-                                    <Path
-                                        fill={NONE}
-                                        strokeWidth={1}
-                                        stroke={PURPLE_DARK}
-                                        d="M12.85,20.21c-4.05,0-7.35-3.3-7.35-7.35s3.3-7.35,7.35-7.35,7.35,3.3,7.35,7.35-3.3,7.35-7.35,7.35Z"
-                                    />
-                                </Svg>
-                            </View>
+                            <LoopIcon color1={PURPLE_DARK} />
                             <TextInput
                                 style={{
                                     flex: 1,
@@ -215,55 +163,7 @@ function App(): JSX.Element {
                                 gap: 10,
                             }}
                         >
-                            <View>
-                                <Svg
-                                    style={{
-                                        opacity: 0.9,
-                                        aspectRatio: 1,
-                                    }}
-                                    viewBox="-3 -3 73.32 73.32"
-                                >
-                                    <Rect
-                                        fill={NONE}
-                                        stroke={PURPLE_DARK}
-                                        strokeWidth={3.8}
-                                        x="7.21"
-                                        y="43.18"
-                                        width="24.14"
-                                        height="24.14"
-                                        rx="6.09"
-                                        ry="6.09"
-                                    />
-                                    <Rect
-                                        fill={NONE}
-                                        stroke={PURPLE_DARK}
-                                        strokeWidth={3.8}
-                                        x="7.21"
-                                        y="14.43"
-                                        width="24.14"
-                                        height="24.14"
-                                        rx="6.09"
-                                        ry="6.09"
-                                    />
-                                    <Rect
-                                        fill={NONE}
-                                        stroke={PURPLE_DARK}
-                                        strokeWidth={3.8}
-                                        x="35.96"
-                                        y="14.43"
-                                        width="24.14"
-                                        height="52.89"
-                                        rx="6.09"
-                                        ry="6.09"
-                                    />
-                                    <Path
-                                        fill={NONE}
-                                        stroke={PURPLE_DARK}
-                                        strokeWidth={3.8}
-                                        d="M58.86,0H8.46C3.79,0,0,3.79,0,8.46v4.25s0,.04,0,.06c.02,2.09,2.82,2.79,3.94,1.02,1.52-2.39,4.19-3.98,7.23-3.98h16.22c1.88,0,3.61.61,5.02,1.63.74.54,1.74.54,2.48,0,1.41-1.03,3.15-1.63,5.02-1.63h16.23c3.04,0,5.71,1.59,7.23,3.98,1.12,1.76,3.92,1.07,3.94-1.02,0-.02,0-.04,0-.06v-4.25c0-4.67-3.79-8.46-8.46-8.46Z"
-                                    />
-                                </Svg>
-                            </View>
+                            <BoxIcon color1={PURPLE_DARK} />
                             <TextInput
                                 style={{
                                     flex: 1,
@@ -300,34 +200,7 @@ function App(): JSX.Element {
                                 padding: 13,
                             }}
                         >
-                            <Svg style={{}} viewBox="-2 -2 101 101">
-                                <Circle stroke={PURPLE_DARK} strokeWidth={3} fill={NONE} cx="48.5" cy="48.5" r="47.5" />
-                                <Polygon
-                                    fill={PURPLE_DARK}
-                                    points="48.5 29.01 33.26 36.35 29.5 52.84 40.04 66.06 56.96 66.06 67.5 52.84 63.74 36.35 48.5 29.01"
-                                />
-                                <Line stroke={PURPLE_DARK} strokeWidth={3} x1="34.77" y1="93.88" x2="67.5" y2="52.84" />
-                                <Line stroke={PURPLE_DARK} strokeWidth={3} x1="4.35" y1="66.06" x2="56.96" y2="66.06" />
-                                <Line stroke={PURPLE_DARK} strokeWidth={3} x1="7.25" y1="24.94" x2="40.04" y2="66.06" />
-                                <Line stroke={PURPLE_DARK} strokeWidth={3} x1="41.2" y1="1.56" x2="29.5" y2="52.84" />
-                                <Line
-                                    stroke={PURPLE_DARK}
-                                    strokeWidth={3}
-                                    x1="80.64"
-                                    y1="13.53"
-                                    x2="33.26"
-                                    y2="36.35"
-                                />
-                                <Line stroke={PURPLE_DARK} strokeWidth={3} x1="95.88" y1="51.83" x2="48.5" y2="29.01" />
-                                <Line
-                                    stroke={PURPLE_DARK}
-                                    strokeWidth={3}
-                                    x1="75.44"
-                                    y1="87.62"
-                                    x2="63.74"
-                                    y2="36.35"
-                                />
-                            </Svg>
+                            <ApertureIcon color1={PURPLE_DARK} />
                         </View>
                     </TouchableOpacity>
                 </SafeAreaView>
---
+++ src/components/Icons.tsx
@@ -0,0 +1,97 @@
+import { View } from 'react-native';
+import Svg, { Circle, Line, Path, Polygon, Rect } from 'react-native-svg';
+import { NONE } from '../util/constants';
+
+type IconProps = {
+    color1: string;
+};
+
+export const LoopIcon = ({ color1 }: IconProps) => (
+    <View>
+        <Svg
+            style={{
+                opacity: 0.9,
+                aspectRatio: 1,
+            }}
+            viewBox="-1 -1 31.71 31.71"
+        >
+            <Path fill={NONE} strokeWidth={2} stroke={color1} d="M30.21,30.21l-9.47-9.47" />
+            <Circle fill={NONE} strokeWidth={2} stroke={color1} cx="12.85" cy="12.85" r="11.35" />
+            <Path
+                fill={NONE}
+                strokeWidth={1}
+                stroke={color1}
+                d="M12.85,20.21c-4.05,0-7.35-3.3-7.35-7.35s3.3-7.35,7.35-7.35,7.35,3.3,7.35,7.35-3.3,7.35-7.35,7.35Z"
+            />
+        </Svg>
+    </View>
+);
+
+export const BoxIcon = ({ color1 }: IconProps) => (
+    <View>
+        <Svg
+            style={{
+                opacity: 0.9,
+                aspectRatio: 1,
+            }}
+            viewBox="-3 -3 73.32 73.32"
+        >
+            <Rect
+                fill={NONE}
+                stroke={color1}
+                strokeWidth={3.8}
+                x="7.21"
+                y="43.18"
+                width="24.14"
+                height="24.14"
+                rx="6.09"
+                ry="6.09"
+            />
+            <Rect
+                fill={NONE}
+                stroke={color1}
+                strokeWidth={3.8}
+                x="7.21"
+                y="14.43"
+                width="24.14"
+                height="24.14"
+                rx="6.09"
+                ry="6.09"
+            />
+            <Rect
+                fill={NONE}
+                stroke={color1}
+                strokeWidth={3.8}
+                x="35.96"
+                y="14.43"
+                width="24.14"
+                height="52.89"
+                rx="6.09"
+                ry="6.09"
+            />
+            <Path
+                fill={NONE}
+                stroke={color1}
+                strokeWidth={3.8}
+                d="M58.86,0H8.46C3.79,0,0,3.79,0,8.46v4.25s0,.04,0,.06c.02,2.09,2.82,2.79,3.94,1.02,1.52-2.39,4.19-3.98,7.23-3.98h16.22c1.88,0,3.61.61,5.02,1.63.74.54,1.74.54,2.48,0,1.41-1.03,3.15-1.63,5.02-1.63h16.23c3.04,0,5.71,1.59,7.23,3.98,1.12,1.76,3.92,1.07,3.94-1.02,0-.02,0-.04,0-.06v-4.25c0-4.67-3.79-8.46-8.46-8.46Z"
+            />
+        </Svg>
+    </View>
+);
+
+export const ApertureIcon = ({ color1 }: IconProps) => (
+    <Svg style={{}} viewBox="-2 -2 101 101">
+        <Circle stroke={color1} strokeWidth={3} fill={NONE} cx="48.5" cy="48.5" r="47.5" />
+        <Polygon
+            fill={color1}
+            points="48.5 29.01 33.26 36.35 29.5 52.84 40.04 66.06 56.96 66.06 67.5 52.84 63.74 36.35 48.5 29.01"
+        />
+        <Line stroke={color1} strokeWidth={3} x1="34.77" y1="93.88" x2="67.5" y2="52.84" />
+        <Line stroke={color1} strokeWidth={3} x1="4.35" y1="66.06" x2="56.96" y2="66.06" />
+        <Line stroke={color1} strokeWidth={3} x1="7.25" y1="24.94" x2="40.04" y2="66.06" />
+        <Line stroke={color1} strokeWidth={3} x1="41.2" y1="1.56" x2="29.5" y2="52.84" />
+        <Line stroke={color1} strokeWidth={3} x1="80.64" y1="13.53" x2="33.26" y2="36.35" />
+        <Line stroke={color1} strokeWidth={3} x1="95.88" y1="51.83" x2="48.5" y2="29.01" />
+        <Line stroke={color1} strokeWidth={3} x1="75.44" y1="87.62" x2="63.74" y2="36.35" />
+    </Svg>
+);
