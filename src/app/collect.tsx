02fcb6b4872d95cfbc186982320887a190b8c153
vinnichase <vincenz.koop@gmail.com>
2025-02-23 13:16:50 +0100

    first blur header with scroll content

--- src/app/collect.tsx
+++ src/app/collect.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
 import { PURPLE_DARK, WHITE } from '../util/constants';
 import { useAtom } from '@gothub-team/got-atom';
 import { ObjectDetectionResultAtom } from '../atoms/ObjectDetectionResultAtom';
 import { ObjectTile } from '../components/ObjectTile';
+import { BlurView } from 'expo-blur';
 
 function App(): JSX.Element {
     const { image, objects } = useAtom(ObjectDetectionResultAtom);
@@ -15,25 +16,42 @@ function App(): JSX.Element {
                 backgroundColor: PURPLE_DARK,
             }}
         >
-            <SafeAreaView
+            <SafeAreaView />
+            <ScrollView style={{ marginTop: 100, overflow: 'visible' }}>
+                <View
+                    style={{
+                        gap: 50,
+                    }}
+                >
+                    {!objects || !image || objects.length === 0 ? (
+                        <Text style={{ marginTop: 40, marginHorizontal: 20, color: WHITE, fontSize: 20 }}>
+                            No Results. Get Back!
+                        </Text>
+                    ) : (
+                        objects.map(({ frame }) => (
+                            <ObjectTile
+                                image={image}
+                                rect={[frame.origin.x, frame.origin.y, frame.size.x, frame.size.y]}
+                            ></ObjectTile>
+                        ))
+                    )}
+                </View>
+            </ScrollView>
+            <SafeAreaView />
+            <BlurView
                 style={{
-                    flex: 1,
-                    gap: 50,
+                    position: 'absolute',
+                    width: '100%',
+                    left: 0,
+                    top: 0,
+                    borderBottomColor: `${WHITE}22`,
+                    borderBottomWidth: 1,
                 }}
             >
-                {!objects || !image || objects.length === 0 ? (
-                    <Text style={{ marginTop: 40, marginHorizontal: 20, color: WHITE, fontSize: 20 }}>
-                        No Results. Get Back!
-                    </Text>
-                ) : (
-                    objects.map(({ frame }) => (
-                        <ObjectTile
-                            image={image}
-                            rect={[frame.origin.x, frame.origin.y, frame.size.x, frame.size.y]}
-                        ></ObjectTile>
-                    ))
-                )}
-            </SafeAreaView>
+                <SafeAreaView style={{ backgroundColor: `${PURPLE_DARK}44` }}>
+                    <View style={{ height: 100 }}></View>
+                </SafeAreaView>
+            </BlurView>
         </View>
     );
 }
