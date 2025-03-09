881a3454bbe551ad413220772a8e295a6de79baa
vinnichase <vincenz.koop@gmail.com>
2025-03-09 18:03:36 +0100

    basic tag edit

--- src/app/categorize.tsx
+++ src/app/categorize.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Image, useWindowDimensions, View } from 'react-native';
-import { BLACK, PURPLE_DARK, PURPLE_LIGHT } from '../util/constants';
+import { Image, TextInput, useWindowDimensions, View } from 'react-native';
+import { BLACK, PURPLE_DARK, PURPLE_LIGHT, PURPLE_MID, WHITE } from '../util/constants';
 import { useAtom } from '@gothub-team/got-atom';
 import { EditObjectsAtom } from '../atoms/EditObjectsAtom';
 
 function App(): JSX.Element {
     const { width } = useWindowDimensions();
     const { index, objects } = useAtom(EditObjectsAtom);
+    const object = objects[index];
 
     return (
         <View
@@ -29,15 +30,40 @@ function App(): JSX.Element {
                         shadowColor: `${PURPLE_LIGHT}`,
                         shadowOpacity: 1,
                         shadowRadius: 100,
+                        gap: 18,
                     }}
                 >
                     <Image
-                        source={{ uri: objects[index]?.uri }}
+                        source={{ uri: object?.uri }}
                         style={{
                             width,
                             height: width,
                         }}
                     ></Image>
+                    {object?.tags?.map((tag, i) => (
+                        <View
+                            key={i}
+                            style={{
+                                height: 45,
+                                marginHorizontal: 18,
+                                padding: 4,
+                                paddingHorizontal: 18,
+                                backgroundColor: PURPLE_LIGHT,
+                                opacity: 0.9,
+                                borderRadius: 14,
+                            }}
+                        >
+                            <TextInput
+                                style={{
+                                    flex: 1,
+                                    height: '100%',
+                                    fontSize: 20,
+                                    color: WHITE,
+                                }}
+                                defaultValue={tag}
+                            />
+                        </View>
+                    ))}
                 </View>
             </View>
         </View>
