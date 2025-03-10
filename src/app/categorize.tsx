0bda96046dae6e444908fd5817a5a51493f8a27a
vinnichase <vincenz.koop@gmail.com>
2025-03-10 17:04:12 +0100

    keyboard view

--- src/app/categorize.tsx
+++ src/app/categorize.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Image, TextInput, useWindowDimensions, View } from 'react-native';
-import { BLACK, PURPLE_DARK, PURPLE_LIGHT, PURPLE_MID, WHITE } from '../util/constants';
+import { Image, Keyboard, KeyboardAvoidingView, ScrollView, TextInput, useWindowDimensions, View } from 'react-native';
+import { BLACK, PURPLE_DARK, PURPLE_LIGHT, WHITE } from '../util/constants';
 import { useAtom } from '@gothub-team/got-atom';
 import { EditObjectsAtom } from '../atoms/EditObjectsAtom';
 
@@ -19,7 +19,8 @@ function App(): JSX.Element {
                 shadowRadius: 50,
             }}
         >
-            <View
+            <KeyboardAvoidingView
+                behavior="height"
                 style={{
                     flex: 1,
                     overflow: 'hidden',
@@ -27,45 +28,58 @@ function App(): JSX.Element {
             >
                 <View
                     style={{
+                        flex: 1,
                         shadowColor: `${PURPLE_LIGHT}`,
                         shadowOpacity: 1,
                         shadowRadius: 100,
-                        gap: 18,
                     }}
+                    onTouchEnd={() => Keyboard.dismiss()}
                 >
                     <Image
                         source={{ uri: object?.uri }}
                         style={{
                             width,
-                            height: width,
+                            height: '50%',
+                            maxHeight: width,
                         }}
                     ></Image>
-                    {object?.tags?.map((tag, i) => (
+                    <ScrollView style={{ flex: 1 }}>
                         <View
-                            key={i}
                             style={{
-                                height: 45,
-                                marginHorizontal: 18,
-                                padding: 4,
-                                paddingHorizontal: 18,
-                                backgroundColor: PURPLE_LIGHT,
-                                opacity: 0.9,
-                                borderRadius: 14,
+                                flex: 1,
+                                gap: 18,
+                                marginVertical: 18,
                             }}
+                            onTouchEnd={(e) => e.stopPropagation()}
                         >
-                            <TextInput
-                                style={{
-                                    flex: 1,
-                                    height: '100%',
-                                    fontSize: 20,
-                                    color: WHITE,
-                                }}
-                                defaultValue={tag}
-                            />
+                            {object?.tags?.map((tag, i) => (
+                                <View
+                                    key={i}
+                                    style={{
+                                        height: 45,
+                                        marginHorizontal: 18,
+                                        padding: 4,
+                                        paddingHorizontal: 18,
+                                        backgroundColor: PURPLE_LIGHT,
+                                        opacity: 0.9,
+                                        borderRadius: 14,
+                                    }}
+                                >
+                                    <TextInput
+                                        style={{
+                                            flex: 1,
+                                            height: '100%',
+                                            fontSize: 20,
+                                            color: WHITE,
+                                        }}
+                                        defaultValue={tag}
+                                    />
+                                </View>
+                            ))}
                         </View>
-                    ))}
+                    </ScrollView>
                 </View>
-            </View>
+            </KeyboardAvoidingView>
         </View>
     );
 }
