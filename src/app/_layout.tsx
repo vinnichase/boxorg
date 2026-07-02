0d53194bd3573dd618081e8749ce76e0e094034f
vinnichase <vincenz.koop@gmail.com>
2026-07-02 14:40:04 +0200

    gesture handler

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,16 @@
 import 'react-native-gesture-handler';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { KeyboardProvider } from 'react-native-keyboard-controller';
 
 export default function RootLayout() {
     return (
-        <KeyboardProvider>
-            <StatusBar style="light" />
-            <Stack screenOptions={{ headerShown: false }} />
-        </KeyboardProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <KeyboardProvider>
+                <StatusBar style="light" />
+                <Stack screenOptions={{ headerShown: false }} />
+            </KeyboardProvider>
+        </GestureHandlerRootView>
     );
 }
