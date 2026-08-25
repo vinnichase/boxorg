bdbaad9cd8c24739c0d19584c5215a5ee11c6cee
vinnichase <vinnichase@noreply.codeberg.org>
2026-08-25 07:45:33 +0200

    fix washed-out screens after Android push navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,13 +3,14 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { KeyboardProvider } from 'react-native-keyboard-controller';
+import { PURPLE_DARK } from '../util/constants';
 
 export default function RootLayout() {
     return (
-        <GestureHandlerRootView style={{ flex: 1 }}>
+        <GestureHandlerRootView style={{ flex: 1, backgroundColor: PURPLE_DARK }}>
             <KeyboardProvider>
                 <StatusBar style="light" />
-                <Stack screenOptions={{ headerShown: false }} />
+                <Stack screenOptions={{ headerShown: false, animation: 'slide_from_right' }} />
             </KeyboardProvider>
         </GestureHandlerRootView>
     );
